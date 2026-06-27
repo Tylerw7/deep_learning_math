2fa0960517f949bd536a384141bf5c0ad75f1d9b
Tylerw7 <t4wettach@gmail.com>
2026-06-26 19:12:39 -0700

    first commit

---
+++ linear_algebra/vectors/main.py
@@ -0,0 +1,12 @@
+
+
+# VECTORS #
+
+# Vectors are represented by the letter "v"
+# Vectors is a straight line with some length and some direction EX: [1,2]
+# it doesnt matter where the vector starts.
+
+# the start of the vector is the tail, and the direction its going is the head.
+# a vector is not the same as a coordinate point [1,2] does not start at at 1.
+
+
