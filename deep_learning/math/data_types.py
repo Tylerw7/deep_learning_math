46870f9f51e7642e215b760fccdb645046e32ab8
Tylerw7 <t4wettach@gmail.com>
2026-06-26 19:51:49 -0700

    update

---
+++ deep_learning/math/data_types.py
@@ -0,0 +1,16 @@
+
+
+# scalar - 7 " a number"
+# vector - on physical dimension
+# matrix - a 2 dimensional array of numbers - EX: a spead sheet
+# any thing high is called a tensor or aka a 3 dimensional matrix
+
+# In PyTorch though all the above are called Tensors to them.
+
+
+
+#---------------------------------
+# HOW TO TURN REALITY INTO NUMBERS
+#---------------------------------
+# TYPES OF REALITY:
+    # Continuous / Categorical
